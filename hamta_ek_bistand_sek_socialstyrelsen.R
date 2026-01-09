08ad3509b5ba300aab62429d020e994245bdf55b
FaluPeppe <larspetermoller@gmail.com>
2026-01-09 13:02:38 +0100

    Tagit bort värden för månader som inte finns publicerad data för.

--- hamta_ek_bistand_sek_socialstyrelsen.R
+++ hamta_ek_bistand_sek_socialstyrelsen.R
@@ -43,9 +43,12 @@ hamta_ek_bistand_sek_socialstyrelsen <- function() {
       mutate({{ enhet_kol }} := na_if(str_replace_all(.data[[enhet_kol]], "--", NA_character_), NA_character_),
              {{ enhet_kol }} := .data[[enhet_kol]] %>% as.numeric(),
              Bakgrund = bakgrund_varde) %>%
-      relocate(Bakgrund, .after = Region)
+      relocate(Bakgrund, .after = Region) %>%
+      relocate({{ enhet_kol }}, .after = last_col())
     )
   }) %>% list_rbind()
+
+  sista_kol <- names(retur_df)[ncol(retur_df)]
 
   regionnyckel <- hamtaregtab() %>%
     rename(Regionkod = regionkod)
@@ -60,6 +63,12 @@ hamta_ek_bistand_sek_socialstyrelsen <- function() {
     relocate(Regionkod, .before = "Region") %>%
     left_join(manadsnyckel, by = "Månad") %>%
     relocate(Månad_num, .after = "Månad")
+
+  # filtrera bort år-månader där samtliga värden är NA (kommande månader)
+  retur_df <- retur_df %>%
+    group_by(År, Månad) %>%
+    filter(!all(across(all_of(sista_kol), is.na))) %>%
+    ungroup()
 
   unlink(tmpdir, recursive = TRUE, force = TRUE)
   return(retur_df)
